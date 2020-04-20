a25955f58d356b50c7b9a5fdcca3869399e82a58
Marat Kalibekov <marat@kalibek.kz>
2020-04-20 15:31:46 +0600

    Initial commit

---
+++ MSignClient/Document1.cs
@@ -0,0 +1,37 @@
+﻿using System.Text.Json.Serialization;
+
+namespace MSignClient
+{
+    public class Document1
+    {
+        public string name { get; set; }
+        public string description { get; set; }
+        public string filename { get; set; }
+        public string mimeType { get; set; }
+        public string base64 { get; set; }
+        public string keyCategory { get; set; }
+        public string signType { get; set; }
+
+        public bool restricted = false;
+
+        public string[] restrictedTo = new string[] { };
+    }
+
+    public class Document1Response
+    {
+        public string id { get; set; }
+        public string url { get; set; }
+        public string md5 { get; set; }
+        public string sha1 { get; set; }
+    }
+
+    public enum KeyCategory
+    {
+        SIGNATURE, AUTHENTICATION
+    }
+
+    public enum SignType
+    {
+        XMLDSIG, PKCS7
+    }
+}
---
+++ MSignClient/MSignClient.cs
@@ -0,0 +1,33 @@
+﻿using RestSharp;
+using RestSharp.Serialization;
+using System;
+
+using Newtonsoft.Json;
+using RestSharp.Serialization;
+
+namespace MSignClient
+{
+    class MSignClient
+    {
+        public static Document1Response document1(string baseUrl, string token, Document1 document1)
+        {
+            var client = new RestClient(baseUrl);
+
+            var request = new RestRequest("documents1", DataFormat.Json);
+            request.Method = Method.POST;
+            request.AddJsonBody(document1);
+            request.AddHeader("AccessToken", token);
+
+            IRestResponse<Document1Response> response = client.Execute<Document1Response>(request);
+
+            if (response.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                return response.Data;
+            }
+            Console.WriteLine(response.Content);
+            Console.WriteLine(response.ErrorMessage);
+
+            return null;
+        }
+    }
+}
